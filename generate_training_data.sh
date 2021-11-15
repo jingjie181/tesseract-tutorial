9204f36359e9ea7bafc32e48851309cc6cfb933e
jingjie181 <jingjie.tian@aimazing.co>
2021-11-15 14:11:35 +0800

    a

--- generate_training_data.sh
+++ generate_training_data.sh
@@ -1,10 +1,2 @@
 rm -rf train/*
-tesstrain.sh --fonts_dir fonts \
-	--fontlist 'Aksaramatee_Bold' \
-	--lang eng \
-	--linedata_only \
-	--langdata_dir langdata_lstm \
-	--tessdata_dir tesseract/tessdata \
-	--save_box_tiff \
-	--maxpages 10 \
-	--output_dir train
+tesstrain.py --fonts_dir fonts --fontlist 'Aksaramatee_Bold' --lang tha --linedata_only --langdata_dir langdata_lstm --tessdata_dir tesseract/tessdata --save_box_tiff --maxpages 10 --output_dir train
